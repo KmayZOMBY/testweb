266a0518eea7dfee0a38e93da96f0e50089d2927
KmayZOMBY <49759642+KmayZOMBY@users.noreply.github.com>
2020-06-02 12:13:07 +0100

    testing VUEjs with Store.js file + HTML tags added

---
+++ src/store.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+  el: '#app-24',
+  data: {
+    producttitle: 'Perspi-Shield 24hr Deodorant',
+    price: '9.5',
+    ingredients: 'alcohol',
+    keys: [
+      { text: '0% Alcohol' },
+      { text: '0% Aluminium' },
+      { text: 'Gentle on skin' },
+      { text: 'Easy application with roll-on' }
+    ]
+  }
+})
+
+var app = new Vue({
+  el: '#app-72',
+  data: {
+    producttitle: 'Perspi-Shield 72hr Deodorant',
+    price: '6.8',
+    ingredients: 'no alcohol',
+    keys: [
+      { text: '0% Alcohol' },
+      { text: 'Lasts up to 72hr' },
+      { text: 'Gentle on skin' },
+      { text: 'Easy application with roll-on' }
+    ]
+  }
+})
