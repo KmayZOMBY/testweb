55b778e4511f1571ab3dc45539021fdf110f004b
KmayZOMBY <49759642+KmayZOMBY@users.noreply.github.com>
2020-06-02 15:04:36 +0100

    added all products + v-bind tests

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 var app = new Vue({
   el: '#app-24',
   data: {
+    image: './images/600PX - 24hr.png',
+    link: 'http://bit.ly/2lvMsAU',
     producttitle: 'Perspi-Shield 24hr Deodorant',
-    price: '9.5',
+    price: '£9.50',
     ingredients: 'alcohol',
     keys: [
       { text: '0% Alcohol' },
@@ -16,14 +18,53 @@ var app = new Vue({
 var app = new Vue({
   el: '#app-72',
   data: {
+    image: './images/600PX - 72hr.png',
+    link: 'http://bit.ly/2lDa4Dq',
     producttitle: 'Perspi-Shield 72hr Deodorant',
-    price: '6.8',
+    price: '£6.80',
     ingredients: 'no alcohol',
     keys: [
       { text: '0% Alcohol' },
       { text: 'Lasts up to 72hr' },
       { text: 'Gentle on skin' },
+      { text: 'Targets body odour bacteria' },
       { text: 'Easy application with roll-on' }
+    ]
+  }
+})
+
+var app = new Vue({
+  el: '#app-wipes',
+  data: {
+    image: './images/600PX - PSWipes.png',
+    link: 'https://www.the-antiperspirant-and-deodorant-company.com/product/perspi-shield-antiperspirant-deodorant-wipes/',
+    producttitle: 'Perspi-Shield Deodorant Wipes',
+    price: '£12.00',
+    ingredients: 'no alcohol',
+    keys: [
+      { text: 'Antiperspirant properties' },
+      { text: 'Lasts up to 24hr' },
+      { text: 'Gentle on skin' },
+      { text: 'Easy to transport with you' },
+      { text: 'Ideal for camping & travel' }
+    ]
+  }
+})
+
+var app = new Vue({
+  el: '#app-liners',
+  data: {
+    image: './images/Perspi-Shield-liners-transparent.png',
+    link: 'http://bit.ly/2lxGOy2',
+    producttitle: 'Perspi-Shield Liners',
+    price: '£4.00',
+    ingredients: 'fabric',
+    keys: [
+      { text: 'All day protection' },
+      { text: 'Suitable for all clothing' },
+      { text: 'Gentle on skin' },
+      { text: 'Easy application on clothes' },
+      { text: 'Discreet and unisex' }
     ]
   }
 })
